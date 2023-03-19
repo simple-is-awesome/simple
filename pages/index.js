7320c3f525b30cde41c6a5d340b4d7bab74d5b97
Jaya <82650452+real-jiakai@users.noreply.github.com>
2023-03-19 13:33:45 +0000

    feat: :sparkles: finish pre-render and data-fetch

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +25,21 @@ export default function Home() {
         <p>
           My major is Computer Science and Technology. I aspire to become a proficient front-end developer in the future.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
---
+++ pages/posts/[id].js
@@ -0,0 +1,13 @@
+import Layout from '../../components/layout';
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+      paths,
+      fallback: false,
+    };
+  }
+
+export default function Post() {
+  return <Layout>...</Layout>;
+}
