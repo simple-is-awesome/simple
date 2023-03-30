020e5a4bc4bffe5541fa164d99fd81f28043cdb2
jiakai <gujiakai28@gmail.com>
2023-03-30 15:30:30 +0800

    :sparkles: add sitemap support

---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	siteUrl: 'https://demo.njxzc.top',
+	generateRobotsTxt: true,
+}
--- next.config.mjs
+++ next.config.mjs
@@ -19,14 +19,6 @@ export default withBundleAnalyzer(
 	withMDX({
 		pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 		reactStrictMode: true,
-		async rewrites() {
-			return [
-				{
-					source: '/sitemap.xml',
-					destination: '/api/sitemap',
-				},
-			]
-		},
 		webpack: (config) => {
 			config.resolve.fallback = { fs: false }
 			return config
