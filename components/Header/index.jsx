5c96a7e87fdb12671303d3ad96bdf3e839a4bfdb
jiakai <gujiakai28@gmail.com>
2023-04-14 16:24:52 +0800

    :bug: improve website performance with lighthouse tips 01

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -10,7 +10,7 @@ export default function Header() {
 		const currentTheme = theme === 'system' ? systemTheme : theme
 		if (currentTheme === 'dark') {
 			return (
-				<button className='bg-gray-300 p-2 rounder-md hover:ring-2 hover:ring-gray-300 dark:bg-gray-600' onClick={() => setTheme('light')}>
+				<button aria-label="Switch to light mode" className='bg-gray-300 p-2 rounder-md hover:ring-2 hover:ring-gray-300 dark:bg-gray-600' onClick={() => setTheme('light')}>
 					<svg svg="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
 						<path fillRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clipRule="evenodd" />
 					</svg>
@@ -18,7 +18,7 @@ export default function Header() {
 			)
 		} else {
 			return (
-				<button className='bg-gray-300 p-2 rounder-md hover:ring-2 hover:ring-gray-300 dark:bg-gray-600' onClick={() => setTheme('dark')}>
+				<button aria-label="Switch to dark mode" className='bg-gray-300 p-2 rounder-md hover:ring-2 hover:ring-gray-300 dark:bg-gray-600' onClick={() => setTheme('dark')}>
 					<svg svg="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" >
 						<path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
 					</svg >
--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html>
-			<Head />
+		<Html lang='en'>
+			<Head>
+				<meta name="description" content="This is a blog demo built with Next.js." />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
