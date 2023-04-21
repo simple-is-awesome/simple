ea4231fe9235d14592ed1a239647979bbf2a19bf
jiakai <gujiakai28@gmail.com>
2023-04-21 09:31:31 +0800

    :bug: try to fix nextjs docker load env error 08

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
 const config = {
+	// basic config
 	baseURL: 'https://demo.njxzc.top',
 	siteTitle: 'Demo website',
 	siteDescription: 'Demo website for Blog System',
 	keyWords: 'blog, demo, website',
 	footer: '© 2023 Made with ❤️ By Jiakai',
 	postsPerPage: 10,
+	githubRepo: 'https://github.com/simple-is-awesome/simple',
+	showComment: true,
+	// third party integration
 	inoreaderChannel: 'https://www.inoreader.com/stream/user/1005341682/tag/user-broadcasted/view/json',
 	raindrop: 'https://bg.raindrop.io/rss/public/32937900',
-	githubRepo: 'https://github.com/simple-is-awesome/simple'
+	// openai
+	openaikeyAvailable: true,
+	// cloudflare
+	cloudflareTurnstileSiteKey: 'xxxxxx',
+	// supabase
+	supabaseAnonKey: 'xxxxxx',
+	supabaseUrl: 'https://xxxxx.supabase.co',
 }
 
 export default config
